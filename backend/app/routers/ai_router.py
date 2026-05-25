5d6ffa6070ec78fcba069bfd6b6a7807c8403af3
Bhavika Vaishnav <bhavikavsnv99@gmail.com>
2026-05-25 16:04:53 +0530

    my commit

--- backend/app/routers/ai_router.py
+++ backend/app/routers/ai_router.py
@@ -1,19 +1,31 @@
-from app.database.mongodb import db
 from fastapi import APIRouter
 from app.schemas.emergency_schema import EmergencyRequest
 from ai_module.agents.emergency_agent import analyze_emergency
+from app.database.mongodb import db
+from datetime import datetime
 
 router = APIRouter()
-
 
 @router.post("/analyze")
 def analyze(data: EmergencyRequest):
 
     result = analyze_emergency(data.message)
 
+    severity = "LOW"
+
+    if "high" in result.lower():
+        severity = "HIGH"
+
+    elif "medium" in result.lower():
+        severity = "MEDIUM"
+
+    timestamp = datetime.now()
+
     db.emergencies.insert_one({
         "user_message": data.message,
-        "ai_response": result
+        "ai_response": result,
+        "severity": severity,
+        "timestamp": timestamp
     })
 
     return {
